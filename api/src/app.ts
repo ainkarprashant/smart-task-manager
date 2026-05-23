bf43c55efa9f61b50db2aabfedf81e41bb785249
Prashant Ainkar <ainkarprashant@gmail.com>
2026-05-23 21:20:48 +0530

    feat: implement task CRUD APIs with auth

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes";
 import { authMiddleware } from "./middleware/auth.middleware";
+import taskRoutes from "./routes/task.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 // ROOT ROUTE
 app.get("/", (req, res) => {
